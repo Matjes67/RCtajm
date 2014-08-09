50b3d72598399ff9ee190e7da19b635d79f97254
condac <burns.nu@gmail.com>
2014-08-09 11:06:27 +0200

    mergeconflict

--- arduinoDecoderDummy/arduinoDecoderDummy.ino
+++ arduinoDecoderDummy/arduinoDecoderDummy.ino
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-int counter = 0;
+
 boolean started = false;
 char inByte;
 int led = 13;
-=======
 int counter = 200;
->>>>>>> 044caa1d1b288f2c1019e01222c9c72eb75c3c26
 
 
 void setup() {
@@ -40,7 +37,7 @@ void loop() {
   
   Serial.println(":E");
   delay(2000);        // delay in between reads for stability
-<<<<<<< HEAD
+
   }
   else {
     digitalWrite(led, LOW);
@@ -56,6 +53,3 @@ void loop() {
     }
     }
 }
-=======
-}
->>>>>>> 044caa1d1b288f2c1019e01222c9c72eb75c3c26
